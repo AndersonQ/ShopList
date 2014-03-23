0ac76b8a5683a7c9fccbc84f8b531cf055bef75e
Anderson de França Queiroz <contato@andersonq.eti.br>
2014-03-23 01:16:16 +0000

    .

--- src/br/eti/andersonq/Update.java
+++ src/br/eti/andersonq/Update.java
@@ -1,13 +1,13 @@
 package br.eti.andersonq;
 
-public interface Update {
-	
-	/*
+public interface Update 
+{
+	/**
 	 * Called when it is necessary to make a update due to any change on Database
 	 */
 	public void updateDisplayedData();
 	
-	/*
+	/**
 	 * Call when it is necessary to update the displayed cost
 	 */
 	public void updateCost();
